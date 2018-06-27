10b68061e1cb65086907ce560eabc7cae22caf7b
Adrian Smith <adrian.smith@keysight.com>
2018-06-27 14:57:40 -0700

    switching to go/swagger based Rest API

--- REST-API/src/constants.js
+++
@@ -1,5 +0,0 @@
-export class Constants {
-  static get dbUrl() {
-    return 'http://10.128.0.7/';
-  }
-}
--- REST-API/src/lib/policies/logRequest.js
+++
@@ -1,6 +0,0 @@
-const _ = require('lodash');
-
-module.exports = (request, reply, next) => {
-  request.log(['debug', 'input'], _.pick(request, ['id', 'params', 'headers', 'info']));
-  next(null, true);
-};
